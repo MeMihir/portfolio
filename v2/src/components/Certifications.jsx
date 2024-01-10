d032a36f908790968e6423bc2d036ee47ad7a95e
Mihir <pavuskarmihir@gmail.com>
2024-01-11 00:36:05 +0530

    add certificate documents

--- v2/src/components/Certifications.jsx
+++ v2/src/components/Certifications.jsx
@@ -1,20 +1,122 @@
 import {
   Box,
+  Button,
+  ButtonGroup,
   Card,
+  CardBody,
   CardHeader,
   Container,
   Divider,
-  Flex,
   HStack,
   Heading,
-  IconButton,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
   Stack,
   Text,
   VStack,
+  useDisclosure,
 } from "@chakra-ui/react";
 import React from "react";
 import { COLOR_SCHEME } from "../utils";
-import { FaExternalLinkAlt } from "react-icons/fa";
+import { FaExternalLinkAlt, FaFilePdf } from "react-icons/fa";
+
+function CertificationCard({ certs }) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const CertificateModal = (
+    <Modal isOpen={isOpen} onClose={onClose} size={"4xl"} >
+      <ModalOverlay />
+      <ModalContent>
+        <ModalCloseButton />
+        <ModalHeader color={"white"}>{certs.certificateName}</ModalHeader>
+        <ModalBody>
+          {/* <Box w={"70vw"} h={"50vh"} > */}
+            <iframe
+              title="certificate"
+              src={certs.document}
+              className="certificate"
+              frameBorder={0}
+            ></iframe>
+          {/* </Box> */}
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+
+  return (
+    <>
+      {CertificateModal}
+      <Box bottom w={"45%"} mx={"1.5%"}>
+        <Card
+          role={"group"}
+          maxW={"330px"}
+          w={"full"}
+          // bg={useColorModeValue('white', 'gray.800')}
+          boxShadow={"2xl"}
+          rounded={"lg"}
+          pos={"relative"}
+          zIndex={1}
+          minH={"15rem"}
+        >
+          <CardHeader>
+            <Box px={2}>
+              <Heading fontSize={"2xl"} fontFamily={"body"} fontWeight={500}>
+                {certs.certificateName}
+              </Heading>
+              <Text color={"gray.400"}>{certs.institution}</Text>
+            </Box>
+          </CardHeader>
+          <CardBody textAlign={"center"}>
+            <VStack>
+              {/* <Image src={certs.image} h={50} /> */}
+              <Text px={2} fontWeight={300}>
+                Date Issued: {certs.dateIssued}
+              </Text>
+              {certs.dateExpires && (
+                <Text px={2} fontWeight={300}>
+                  Date Expired: {certs.dateExpires}
+                </Text>
+              )}
+              {certs.credentials && (
+                <Text px={2} fontWeight={300}>
+                  Credentials: {certs.credentials}
+                </Text>
+              )}
+              <ButtonGroup variant="ghost" isAttached size={"xs"}>
+                {certs.link && (
+                  <Button
+                    as={"a"}
+                    href={certs.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    colorScheme={COLOR_SCHEME}
+                    rightIcon={<FaExternalLinkAlt />}
+                  >
+                    Verify Credential
+                  </Button>
+                )}
+                <Button
+                  href={certs.docuemnt}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  colorScheme={COLOR_SCHEME}
+                  rightIcon={<FaFilePdf />}
+                  onClick={onOpen}
+                >
+                  View Certificate
+                </Button>
+              </ButtonGroup>
+            </VStack>
+          </CardBody>
+        </Card>
+      </Box>
+    </>
+  );
+}
 
 export default function Certifications({ certifications }) {
   return (
@@ -34,53 +136,9 @@ export default function Certifications({ certifications }) {
           </HStack>
           <Divider orientation="horizontal" />
         </Stack>
-        <HStack px={4} spacing={4} flexWrap="wrap" >
+        <HStack px={4} spacing={4} flexWrap="wrap">
           {certifications.map((certs, index) => (
-            <Box bottom key={index} w={"45%"} mx={"1.5%"} >
-              <Card
-                role={"group"}
-                maxW={"330px"}
-                w={"full"}
-                // bg={useColorModeValue('white', 'gray.800')}
-                boxShadow={"2xl"}
-                rounded={"lg"}
-                pos={"relative"}
-                zIndex={1}
-								minH={"15rem"}
-              >
-                <CardHeader textAlign={"center"}>
-                  <VStack>
-                    {/* <Image src={certs.image} h={50} /> */}
-                    <Box px={2}>
-                      <Heading
-                        fontSize={"2xl"}
-                        fontFamily={"body"}
-                        fontWeight={500}
-                      >
-                        {certs.certificateName}
-                      </Heading>
-                      <Text>{certs.institution}</Text>
-                      <IconButton
-                        // as={Link}
-                        href={certs.link}
-                        target="_blank"
-                        aria-label={`${certs.certificateName}`}
-                        icon={<FaExternalLinkAlt />}
-                        size="xs"
-                        variant="ghost"
-                        colorScheme={COLOR_SCHEME}
-                      />
-                    </Box>
-                    <Text px={2} fontWeight={300}>
-                      {certs.dateIssued}
-                    </Text>
-                    <Text px={2} fontWeight={300}>
-                      {certs.score}
-                    </Text>
-                  </VStack>
-                </CardHeader>
-              </Card>
-            </Box>
+            <CertificationCard certs={certs} key={index} />
           ))}
         </HStack>
       </Stack>
